2d57060301db24ec3a74cecfe516cdef7331df72
Henning Surmeier <me@hensur.de>
2018-06-06 18:40:21 +0200

    Slack Provider: New SecondAttempt and GetLoginURL
    This commit implements the previous changes in the slack Provider.
    This finally enables proper authentication against slack with every
    possible scope by going through the flow two times. First with the
    identity scope, then again with every other possible scope.
    
    The slack-group feature finally works now.

--- providers/slack.go
+++ providers/slack.go
@@ -42,6 +42,10 @@ type SlackGroupItem struct {
 	Name string
 }
 
+type SlackAuthTestResponse struct {
+	OK bool
+}
+
 func NewSlackProvider(p *ProviderData) *SlackProvider {
 	p.ProviderName = "slack"
 	if p.LoginURL == nil || p.LoginURL.String() == "" {
@@ -89,7 +93,7 @@ func (p *SlackProvider) SetGroupID(group string) {
 	}
 }
 
-func (p *SlackProvider) getEndpoint(endpointName string, accessToken string, params url.Values, responseItem interface{}) error {
+func (p *SlackProvider) getEndpoint(endpointName string, accessToken string, params url.Values, responseItem interface{}) (*http.Response, error) {
 	if params != nil {
 		params.Add("token", accessToken)
 	} else {
@@ -106,28 +110,28 @@ func (p *SlackProvider) getEndpoint(endpointName string, accessToken string, par
 	req, _ := http.NewRequest("GET", endpoint.String(), nil)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if resp.StatusCode != 200 {
-		return fmt.Errorf(
+		return nil, fmt.Errorf(
 			"got %d from %q %s", resp.StatusCode, endpoint.String(), body)
 	}
 
 	if err := json.Unmarshal(body, responseItem); err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return resp, nil
 }
 
 func (p *SlackProvider) getIdentity(accessToken string) (*SlackUserIdentityResponse, error) {
 	var userIdentity SlackUserIdentityResponse
-	if err := p.getEndpoint("users.identity", accessToken, nil, &userIdentity); err != nil {
+	if _, err := p.getEndpoint("users.identity", accessToken, nil, &userIdentity); err != nil {
 		return nil, err
 	}
 
@@ -139,7 +143,7 @@ func (p *SlackProvider) getIdentity(accessToken string) (*SlackUserIdentityRespo
 
 func (p *SlackProvider) getGroups(accessToken string) (*SlackGroupListResponse, error) {
 	var groupList SlackGroupListResponse
-	if err := p.getEndpoint("groups.list", accessToken, url.Values{
+	if _, err := p.getEndpoint("groups.list", accessToken, url.Values{
 		"exclude_archived": {"true"},
 		"exclude_members":  {"true"},
 	}, &groupList); err != nil {
@@ -197,11 +201,35 @@ func (p *SlackProvider) GetEmailAddress(s *SessionState) (string, error) {
 	return "", nil
 }
 
-func (p *SlackProvider) SecondAttempt() bool {
-	fmt.Println(p.GroupID)
-	if strings.Contains(p.Scope, "groups:read") || p.GroupID == "" {
+// SecondAttempt checks what scopes our token has by requesting a test endpoint and checking the headers
+// Returns true until all scopes are there
+func (p *SlackProvider) SecondAttempt(session *SessionState) bool {
+	resp, err := p.getEndpoint("auth.test", session.AccessToken, nil, &SlackAuthTestResponse{})
+	if err != nil {
 		return false
 	}
-	p.Scope = "groups:read"
+	if strings.Contains(resp.Header.Get("X-Oauth-Scopes"), "groups") || p.GroupID == "" {
+		return false
+	}
 	return true
+}
+
+// GetLoginURL with typical oauth parameters
+// Requests a different scope on the second try since slack won't allow identity and "normal" scopes in the same request
+func (p *SlackProvider) GetLoginURL(redirectURI, state string, second bool) string {
+	var a url.URL
+	a = *p.LoginURL
+	params, _ := url.ParseQuery(a.RawQuery)
+	params.Set("redirect_uri", redirectURI)
+	params.Set("approval_prompt", p.ApprovalPrompt)
+	if second {
+		params.Set("scope", "groups:read")
+	} else {
+		params.Add("scope", p.Scope)
+	}
+	params.Set("client_id", p.ClientID)
+	params.Set("response_type", "code")
+	params.Add("state", state)
+	a.RawQuery = params.Encode()
+	return a.String()
 }
